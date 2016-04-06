c0d63590d2cdee3f1c5252912d50fa0fc61a3cef
Ivan Bodrov <ibodrov@gmail.com>
2016-04-06 22:35:25 +0300

    task-based concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(test)]
 extern crate test;
+extern crate scoped_threadpool;
 
 use std::vec::Vec;
 use std::collections::HashMap;
@@ -8,6 +9,7 @@ use std::any::{Any, TypeId};
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 struct Entity(u64);
 
+#[derive(Clone, Copy)]
 struct Position {
     x: u32,
     y: u32,
@@ -48,13 +50,13 @@ impl World {
         m.get_mut(&t).unwrap()
     }
 
-    fn add_component<T: Any>(&mut self, e: Entity, c: T) {
+    fn add_component<T: Any + Clone>(&mut self, e: Entity, c: T) {
         let t = TypeId::of::<T>();
         let cs = self.ensure_components(t);
         cs.insert(e, Box::new(c));
     }
 
-    fn component_mut<T: Any>(&mut self, e: &Entity) -> Option<&mut T> {
+    fn component_mut<T: Any + Clone>(&mut self, e: &Entity) -> Option<&mut T> {
         let t = TypeId::of::<T>();
         if let Some(cs) = self.components.get_mut(&t) {
             match cs.get_mut(e) {
@@ -65,24 +67,69 @@ impl World {
             None
         }
     }
+
+    fn components<T: Any + Clone>(&self, es: &[Entity]) -> Vec<T> {
+        let t = TypeId::of::<T>();
+        if let Some(cs) = self.components.get(&t) {
+            es.iter().filter_map(|e| {
+                cs.get(e).map(|c| (*(**c).downcast_ref::<T>().unwrap()).clone())
+            }).collect()
+        } else {
+            Vec::new()
+        }
+    }
+}
+
+struct Task<T: Any + Clone> {
+    cs: Vec<T>,
 }
 
 fn main() {
 }
 
+const ENTITY_COUNT: usize = 100000;
+
 #[bench]
 fn bench(b: &mut test::Bencher) {
     let mut w = World::new();
 
-    for _ in 0..100000 {
+    for _ in 0..ENTITY_COUNT {
+        let e = w.create_entity();
+        w.add_component(e, Position { x: 0, y: 0, z: 0 });
+    }
+
+    let thread_count = 4;
+    let chunks = 128;
+    let mut pool = scoped_threadpool::Pool::new(thread_count);
+
+    b.iter(|| {
+        pool.scoped(|scope|{
+            for es in w.entities.chunks(chunks) {
+                let t = Task { cs: w.components::<Position>(es) };
+                scope.execute(move || {
+                    for ref mut c in t.cs {
+                        c.x += 1;
+                    }
+                });
+            }
+        });
+    });
+}
+
+#[bench]
+fn bench_single_thread(b: &mut test::Bencher) {
+    let mut w = World::new();
+
+    for _ in 0..ENTITY_COUNT {
         let e = w.create_entity();
         w.add_component(e, Position { x: 0, y: 0, z: 0 });
     }
 
     b.iter(|| {
         for e in w.entities.clone() {
-            let p = w.component_mut::<Position>(&e).unwrap();
-            p.x += 1;
+            let c = w.component_mut::<Position>(&e).unwrap();
+            c.x += 1;
         }
     });
 }
+
